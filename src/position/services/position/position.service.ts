97e42e7b0545ca2b8c85cbcb39f857604b3f8cc6
weatherblocker@gmail.com <40629098+AlxMial@users.noreply.github.com>
2022-04-05 10:51:24 +0700

    commit

--- src/position/services/position/position.service.ts
+++ src/position/services/position/position.service.ts
@@ -10,99 +10,112 @@ import { Repository, Connection } from 'typeorm';
 
 @Injectable()
 export class PositionService {
-    constructor(
-        @InjectRepository(tbPosition)
-        private readonly positionRepository: Repository<tbPosition>,
-        private readonly connection: Connection,
-    ) { }
+  constructor(
+    @InjectRepository(tbPosition)
+    private readonly positionRepository: Repository<tbPosition>,
+    private readonly connection: Connection,
+  ) {}
 
-    async getList(params: getDto) {
-        const position = this.positionRepository;
-        try {
-            const pos = await this.positionRepository.find({ isDeleted: false, companyId: params.companyId });
-            return StatusMessage(true, null, pos);
-        } catch (e) {
-            return StatusMessage(false, (e as Error).message, position);
-        }
+  async getList(params: getDto) {
+    const position = this.positionRepository;
+    try {
+      const pos = await this.positionRepository.find({
+        isDeleted: false,
+        companyId: params.companyId,
+      });
+      return StatusMessage(true, null, pos);
+    } catch (e) {
+      return StatusMessage(false, (e as Error).message, position);
     }
+  }
 
-    async getByCompanyId(id: any) {
-        const position = this.positionRepository;
-        try {
-            const data = await this.positionRepository.find({ id: id });
-            return StatusMessage(true, null, data);
-        } catch (e) {
-            return StatusMessage(false, (e as Error).message, position);
-        }
+  async getByCompanyId(id: any) {
+    const position = this.positionRepository;
+    try {
+      const data = await this.positionRepository.find({ id: id });
+      return StatusMessage(true, null, data);
+    } catch (e) {
+      return StatusMessage(false, (e as Error).message, position);
     }
+  }
 
-    async getById(id: number, companyId: number) {
-        try {
-            const data = await this.positionRepository.findOne({
-                id: id,
-                companyId: companyId,
-            });
-            return StatusMessage(true, null, data);;
-        } catch (e) {
-            return { message: (e as Error).message };
-        }
+  async getById(id: number, companyId: number) {
+    try {
+      const data = await this.positionRepository.findOne({
+        id: id,
+        companyId: companyId,
+      });
+      return StatusMessage(true, null, data);
+    } catch (e) {
+      return { message: (e as Error).message };
     }
+  }
 
-    async create(createPosition: positionDto) {
-        const position = this.positionRepository;
-        try {
-            stampAudit(createPosition);
-            const newPosition = this.positionRepository.create(createPosition);
-            const SavePosition = await this.positionRepository.save(newPosition);
-            return StatusMessage(true, null, SavePosition);
-        } catch (e) {
-            return StatusMessage(false, (e as Error).message, position);
-        }
+  async create(createPosition: positionDto) {
+    const position = this.positionRepository;
+    try {
+      stampAudit(createPosition);
+      const newPosition = this.positionRepository.create(createPosition);
+      const SavePosition = await this.positionRepository.save(newPosition);
+      return StatusMessage(true, null, SavePosition);
+    } catch (e) {
+      return StatusMessage(false, (e as Error).message, position);
     }
+  }
 
-    async update(updatePosition: positionDto) {
-        const position = this.positionRepository;
-        try {
-            const data = await this.positionRepository.findOne({ id: updatePosition.id, companyId: updatePosition.companyId });
-            data.positionCode = updatePosition.positionCode;
-            data.positionName = updatePosition.positionName;
-            data.positionNameEn = updatePosition.positionNameEn;
-            data.mainPositionId = updatePosition.mainPositionId;
-            data.description = updatePosition.description;
-            stampAudit(data, updatePosition, 'update');
+  async update(updatePosition: positionDto) {
+    const position = this.positionRepository;
+    try {
+      const data = await this.positionRepository.findOne({
+        id: updatePosition.id,
+        companyId: updatePosition.companyId,
+      });
+      data.positionCode = updatePosition.positionCode;
+      data.positionName = updatePosition.positionName;
+      data.positionNameEn = updatePosition.positionNameEn;
+      data.mainPositionId = updatePosition.mainPositionId;
+      data.description = updatePosition.description;
+      stampAudit(data, updatePosition, 'update');
+      return StatusMessage(
+        true,
+        null,
+        await this.positionRepository.save(data),
+      );
+    } catch (e) {
+      return StatusMessage(false, (e as Error).message, position);
+    }
+  }
+
+  async delete(data: deleteDto) {
+    const position = this.positionRepository;
+    try {
+      for (let i = 0; i < data.id.length; i++) {
+        const result = await this.connection.query(
+          "up_selectAllUse @SearchStr='" +
+            data.id[i] +
+            "',@Column='empId', @companyId='" +
+            data.companyId +
+            "'",
+        );
+        if (result && result.length > 0) {
+          return { message: 'Data is used' };
+        } else {
+          const deletePosition = await this.positionRepository.findOne({
+            id: data.id[i],
+            companyId: data.companyId,
+          });
+          if (deletePosition) {
+            stampAudit(deletePosition, data, 'update', true);
             return StatusMessage(
-                true,
-                null,
-                await this.positionRepository.save(data),
+              true,
+              null,
+              await this.positionRepository.save(deletePosition),
             );
-        } catch (e) {
-            return StatusMessage(false, (e as Error).message, position);
+          }
         }
+      }
+    } catch (e) {
+      return StatusMessage(false, (e as Error).message, position);
     }
-
-    async delete(data: deleteDto) {
-        const position = this.positionRepository;
-        try {
-            for (let i = 0; i < data.id.length; i++) {
-                const result = await this.connection.query(
-                    "up_selectAllUse @SearchStr='" + data.id[i] + "',@Column='empId', @companyId='" + data.companyId + "'",
-                );
-                if (result && result.length > 0) {
-                    return { message: 'Data is used' };
-                } else {
-                    const deletePosition = await this.positionRepository.findOne({ id: data.id[i], companyId: data.companyId });
-                    if (deletePosition) {
-                        stampAudit(deletePosition, data, 'update', true);
-                        return StatusMessage(
-                            true,
-                            null,
-                            await this.positionRepository.save(deletePosition),
-                        );
-                    }
-                }
-            }
-        } catch (e) {
-            return StatusMessage(false, (e as Error).message, position);
-        }
-    }
+  }
 }
